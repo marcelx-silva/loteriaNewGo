58efb6f821827df4d2f7b9c0bf06ad80067872ec
Thiago <thiago.silvamarcelo56@gmail.com>
2022-11-22 11:53:38 -0300

    [FEAT] add jogo domain

---
+++ src/main/java/dominio/jogo/Jogo.java
@@ -0,0 +1,46 @@
+package dominio.jogo;
+
+import dominio.loteria.Loteria;
+import dominio.participante.Participante;
+import exceptions.InsufficientMoney;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static utils.GerarListaNumeros.gerarListaDeNumeros;
+
+public class Jogo {
+    private final Participante participante;
+    private final Map<Integer, List<Integer>> jogos;
+    private final Loteria loteria;
+
+    public Jogo(Participante participante, Loteria loteria) throws InsufficientMoney {
+        this.jogos = new HashMap<>();
+        this.participante = participante;
+        this.loteria = loteria;
+        gerarJogos();
+    }
+
+    private void gerarJogos() throws InsufficientMoney {
+        Integer quantJogos = calcularQuantJogos
+                (this.participante.getValorPago(),this.loteria.getValorJogoLoteria());
+
+        for(Integer i = 1;i<=quantJogos;i++){
+            jogos.put(i,gerarListaDeNumeros(loteria.getQuantValoresSeremSorteados()));
+        }
+    }
+
+    private Integer calcularQuantJogos(Double valorPagoPeloJogador, Double valorJogoLoteria) throws InsufficientMoney {
+        verificarQuantiaDinheiroEValida(valorPagoPeloJogador,valorPagoPeloJogador);
+        int quantidadeJogos = (int) (valorPagoPeloJogador/valorJogoLoteria);
+        participante.setQuantidadeJogoComprados(quantidadeJogos);
+        return quantidadeJogos;
+    }
+
+    private void verificarQuantiaDinheiroEValida(Double valorPagoPeloJogador, Double valorJogoLoteria) throws InsufficientMoney {
+        if (valorPagoPeloJogador<valorJogoLoteria){
+            throw new InsufficientMoney("Quantia insuficiente para comprar um jogo!");
+        }
+    }
+}
